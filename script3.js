6f186dddbd62ee814a89da3225028f9c47878c53
drmeloy <drmeloy@gmail.com>
2020-04-17 10:34:09 -0700

    fs unlink

--- script3.js
+++ script3.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+// READ
 fs.readFile('./hello.txt', (err, data) => {
   if (err) console.log('ERROR');
   console.log('1', data.toString());
@@ -8,6 +9,17 @@ fs.readFile('./hello.txt', (err, data) => {
 const file = fs.readFileSync('./hello.txt');
 console.log('2', file.toString());
 
-fs.appendFile('./hello.txt', ' General Kenobi', err => {
+// APPEND
+// fs.appendFile('./hello.txt', ' General Kenobi', err => {
+//   if (err) console.log(err);
+// });
+
+// WRITE
+// fs.writeFile('bye.text', 'Sad to see you go', err => {
+//   if(err) console.log(err);
+// });
+
+// DELETE
+fs.unlink('./bye.text', err => {
   if (err) console.log(err);
 });
