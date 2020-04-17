eb93a116ad3b157e503acbe9ee89fe1bb352e6e5
drmeloy <drmeloy@gmail.com>
2020-04-17 10:25:49 -0700

    difference between readFile and readFileSync

--- script3.js
+++ script3.js
@@ -2,5 +2,8 @@ const fs = require('fs');
 
 fs.readFile('./hello.txt', (err, data) => {
   if (err) console.log('ERROR');
-  console.log(data.toString());
+  console.log('1', data.toString());
 });
+
+const file = fs.readFileSync('./hello.txt');
+console.log('2', file.toString());
