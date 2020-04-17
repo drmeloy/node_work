1bcf5142527263b9f258891be393c9788141b13f
drmeloy <drmeloy@gmail.com>
2020-04-17 11:45:39 -0700

    part two complete

--- santa_problem/index.js
+++ santa_problem/index.js
@@ -5,11 +5,19 @@ console.time('SantaProblem');
 const instructions = fs.readFileSync('./instructions.txt').toString();
 
 let floor = 0;
+let basement;
+let neverUnderground = true;
 
-for(const char of instructions){
-  if (char === '(') floor++;
-  if (char === ')') floor--;
+for(let i = 0; i < instructions.length; i++){
+  if (instructions[i] === '(') floor++;
+  if (instructions[i] === ')') floor--;
+  if(neverUnderground === true && floor < 0){
+    basement = i + 1;
+    neverUnderground = false;
+  };
 };
 
 console.log(floor);
+console.log(basement);
+console.log(instructions.length);
 console.timeEnd('SantaProblem');
